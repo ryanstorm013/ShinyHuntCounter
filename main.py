0b59a55dce05303bafc30abf1e808a8451769989
ryanstorm <ryanstorm013@gmail.com>
2024-01-12 14:16:03 -0800

    Added More Buttons

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import customtkinter as ctk
 
 ctk.set_appearance_mode("dark")  # Modes: system (default), light, dark
 ctk.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
-
+ctk.set_window_scaling(1)
 
 class TextBox():
     pass
@@ -13,29 +13,43 @@ class MyFrame(ctk.CTkTabview):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
         custom_font =("Times",50,'bold')
+        custom_font2 =("Times",30,'bold')
         # add widgets onto the frame, for example:
         self.label = ctk.CTkLabel(self, text="")
         self.label.grid(row=0, column=0, padx=0, pady=0, sticky="esn")
         
-        self.add("tab 1")
-        self.add("tab 2")
+        self.add("Counter")
+        self.add("Timer")
 
-        text_var = tk.StringVar(value="0")
+        text_var = ctk.StringVar(value="0")
 
-        label = ctk.CTkLabel(master=self.tab("tab 1"),
+        label = ctk.CTkLabel(master=self.tab("Counter"),
                                textvariable=text_var,
                                font=custom_font,
                                width=250,
                                height=150,
                                fg_color=("white", "black")
                                 )
-        label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+        label.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
 
-                
-        button = ctk.CTkButton(self.tab("tab 1"), text="+", width=30, height=25)
+        label2 = ctk.CTkLabel(master=self.tab("Timer"),
+                               textvariable=text_var,
+                               font=custom_font,
+                               width=250,
+                               height=150,
+                               fg_color=("white", "black")
+                                )
         
+        label2.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
+                
+        button = ctk.CTkButton(self.tab("Counter"), text="+", font=custom_font2, width=40, height=35)
+        button2 = ctk.CTkButton(self.tab("Counter"), text="-", font=custom_font2, width=40, height=35)
+        button3 = ctk.CTkButton(self.tab("Counter"), text="Color", font=custom_font2, width=40, height=35)
         # button.grid(row=0, column=0, padx=0, pady=0, sticky="n")
-        button.place(relx=0.5, rely=1.0, anchor=tk.S)
+        button.place(relx=0.8, rely=1.0, anchor=tk.SW)
+        button2.place(relx=0.2, rely=1.0, anchor=tk.SE)
+        button3.place(relx=0.5, rely=1.0, anchor=tk.S)
+        
         
 
         
@@ -47,8 +61,7 @@ class MyFrame(ctk.CTkTabview):
 class App(ctk.CTk, tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        self.geometry("500x300")
-        self.pack
+        self.geometry("600x300")
         self.grid_rowconfigure((0, 1), weight=1)  # configure grid system
         self.grid_columnconfigure(0, weight=1) 
         
@@ -60,6 +73,13 @@ class App(ctk.CTk, tk.Frame):
         button = ctk.CTkButton(self, text="CTkButton", command= self.button_function)
         # button.grid_rowconfigure(0, weight=1)
         button.grid(row=1, column=0, padx=80, pady=0, sticky="wn")
+
+        button2 = ctk.CTkButton(self, text="Flip", command= self.button_function)
+        button2.grid(row=0, column=1, padx=80, pady=0)
+
+        # button3 = ctk.CTkButton(self, text="CTkButton", command= self.button_function)
+        # button3.grid(row=0, column=1, padx=120, pady=0)
+       
         
 
 
