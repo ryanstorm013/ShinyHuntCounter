e7890c78bac67336f2ab22005ce66678e8deab2a
ryanstorm <ryanstorm013@gmail.com>
2024-01-14 10:04:21 -0800

    Added More Buttons and Better Layout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import customtkinter as ctk
+from PIL import Image
+# import ttkbootstrap as ttk
 
 
 ctk.set_appearance_mode("dark")  # Modes: system (default), light, dark
@@ -9,14 +11,46 @@ ctk.set_window_scaling(1)
 class TextBox():
     pass
 
-class MyFrame(ctk.CTkTabview):
+class MyLabel(ctk.CTkLabel):
+    def __init__(self, master):
+        pass
+    pass
+
+
+class MyFrame(ctk.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        # add widgets onto the frame, for example:
+        self.label = ctk.CTkLabel(self, text="")
+        self.label.grid(row=1, column=0, padx=0, pady= (80, 0), sticky="sew")
+
+        entry = ctk.CTkEntry(self, placeholder_text="CTkEntry", width=70)
+        entry.grid(row=0, column=0, padx=20, pady=10)
+        entry.configure(state="disabled")
+
+        entry2 = ctk.CTkEntry(self, placeholder_text="CTkEntry", width=70)
+        entry2.grid(row=0, column=1, padx=20, pady=10)
+        entry2.configure(state="disabled")
+
+        button = ctk.CTkButton(self, text="Flip", width=40, height=35)
+        button.grid(row=1, column=0, padx=30, pady=10)
+
+        button2 = ctk.CTkButton(self, text="Roll", width=40, height=35)
+        button2.grid(row=1, column=1, padx=30, pady=10)
+
+
+
+
+
+class TabView(ctk.CTkTabview):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
         custom_font =("Times",50,'bold')
         custom_font2 =("Times",30,'bold')
         # add widgets onto the frame, for example:
         self.label = ctk.CTkLabel(self, text="")
-        self.label.grid(row=0, column=0, padx=0, pady=0, sticky="esn")
+        self.label.grid(row=0, column=0, padx=(20, 20), pady=0, sticky="sn")
         
         self.add("Counter")
         self.add("Timer")
@@ -62,11 +96,11 @@ class App(ctk.CTk, tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.geometry("600x300")
-        self.grid_rowconfigure((0, 1), weight=1)  # configure grid system
-        self.grid_columnconfigure(0, weight=1) 
+        self.grid_rowconfigure((0, 2), weight=1)  # configure grid system
+        self.grid_columnconfigure(1, weight=1) 
         
 
-        self.my_frame = MyFrame(self)
+        self.my_frame = TabView(self)
         self.my_frame.grid(row=0, column=0, padx=0, pady=0, sticky="w")
         #button
         
@@ -74,12 +108,13 @@ class App(ctk.CTk, tk.Frame):
         # button.grid_rowconfigure(0, weight=1)
         button.grid(row=1, column=0, padx=80, pady=0, sticky="wn")
 
-        button2 = ctk.CTkButton(self, text="Flip", command= self.button_function)
-        button2.grid(row=0, column=1, padx=80, pady=0)
+        # button2 = ctk.CTkButton(self, text="Flip", command= self.button_function, width=40, height=35)
+        # button2.grid(row=0, column=1, padx=80, pady=0)
 
-        # button3 = ctk.CTkButton(self, text="CTkButton", command= self.button_function)
-        # button3.grid(row=0, column=1, padx=120, pady=0)
-       
+        # button3 = ctk.CTkButton(self, text="Roll", command= self.button_function, width=40, height=35)
+        self.other_frame = MyFrame(self)
+        self.other_frame.grid(row=0, column=1, padx=0, pady=0, sticky="s")
+        
         
 
 
@@ -89,7 +124,7 @@ class App(ctk.CTk, tk.Frame):
 
 
 app = App()
-# app.master.title("taco time")
+app.title('Taco Bell')
 
 app.mainloop()
 
